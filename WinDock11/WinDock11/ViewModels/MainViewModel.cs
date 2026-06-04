49bd632f8f2086de744c24bf20fbf46bfe6cde6a
Jeffrey Ghinn <jeffreyghinn@outlook.com>
2026-06-04 19:42:49 +0000

    Added some example DockItems to MainViewModel

--- WinDock11/WinDock11/ViewModels/MainViewModel.cs
+++ WinDock11/WinDock11/ViewModels/MainViewModel.cs
@@ -6,4 +6,28 @@ namespace WinDock11.ViewModels;
 public class MainViewModel
 {
     public ObservableCollection<DockItem> DockItems { get; } = new();
+
+    public MainViewModel()
+    {
+        DockItems.Add(new DockItem
+        {
+            Name = "Chrome",
+            Path = @"C:\Program Files\Google\Chrome\Application\chrome.exe",
+            IsPinned = true
+        });
+
+        DockItems.Add(new DockItem
+        {
+            Name = "Explorer",
+            Path = "explorer.exe:,
+            IsPinned = true
+        });
+
+        DockItems.Add(new DockItem
+        {
+            Name = "Notepad",
+            Path = "notepad.exe",
+            IsPinned = true
+        });
+    }
 }
