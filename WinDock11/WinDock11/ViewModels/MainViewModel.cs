6001e3199e728058fa592c209a4f347bc76e3d86
Jeffrey Ghinn <jeffreyghinn@outlook.com>
2026-06-04 15:03:40 -0400

    Added initial code to MainViewModel.cs

--- WinDock11/WinDock11/ViewModels/MainViewModel.cs
+++ WinDock11/WinDock11/ViewModels/MainViewModel.cs
@@ -1,1 +1,9 @@
-﻿
+﻿using System.Collections.ObjectModel;
+using WinDock11.Models;
+
+namespace WinDock11.ViewModels;
+
+public class MainViewModel
+{
+    public ObservableCollection<DockItem> DockItems { get; } = new();
+}
